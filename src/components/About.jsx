5de7c1e452491c1a403f64e4bbebadd1f9e5e273
Amritendu Mohan Roy <73495744+ItzAmrit2002@users.noreply.github.com>
2023-09-08 00:54:31 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -72,9 +72,9 @@ const About = () => {
                         About Me
                     </h1>
                     <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis dolor earum mollitia labore, unde amet soluta voluptates quos deserunt sapiente.
+                        I am a final year Computer Science student at Netaji Subhash Engineering College, Kolkata. Throughout my academic journey, I have gained extensive expertise in web development, specifically in the MERN stack. I have completed several projects in this field, showcasing my proficiency in various programming languages and frameworks such as C, C++, Java, Python, and Javascript. My passion lies in utilizing innovative technologies to solve real-world problems, and I am eager to find opportunities where I can apply my skills, expand my knowledge, and continue learning.
                     </p>
-                    <a href="https://drive.google.com/file/d/1-TOTRFtcfPeo0s2c7XMEmifyLGkRM1QQ/view?usp=sharing" target="_blank"><button>Resume</button></a>
+                    <a href="https://www.linkedin.com/in/amritendu-mohan-roy-4520181b6/overlay/1635539906283/single-media-viewer/?profileId=ACoAADIxKX4B6naDjou67hadjDt0TxCxwXKRkxQ" target="_blank"><button>Resume</button></a>
                 </motion.div>
             </div>
         </div>
